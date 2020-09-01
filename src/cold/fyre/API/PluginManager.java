e436cb8233a6fc387b8230c4e0bdfd8a28bcd919
user <user@Sommod>
2020-09-01 12:22:52 -0400

    Added Javadoc to all methods

--- src/cold/fyre/API/PluginManager.java
+++ src/cold/fyre/API/PluginManager.java
@@ -100,14 +100,47 @@ public abstract class PluginManager<J extends JavaPlugin> {
 	 */
 	public Logger getLogger() { return plugin.getLogger(); }
 	
+	/**
+	 * Logs a Standard info message to the Console. This uses the default
+	 * logger of the server obtained from the plugin stored within this class.
+	 * If you want to use the Minecraft Color Codes, use the method {@link #getConsole()}.
+	 * @param message - Text to display via console.
+	 */
 	public void logMessage(String message) { getLogger().log(Level.INFO, message); }
 	
+	/**
+	 * Similar to the {@link #logMessage(String)} method, this allows you to set the Level of the text
+	 * when sending it to the Console. To use the Minecraft Color Codes, use the {@link #getConsole()}
+	 * method.
+	 * @param level - Level of message
+	 * @param message - Text to display via console.
+	 */
 	public void logMessage(Level level, String message) { getLogger().log(level, message); }
 	
+	/**
+	 * This obtains a new instance of the default config.yml file found within the base folder
+	 * of the plugin. This means that when data is changed via Console or in-game, the data
+	 * will be updated in the plugin as well. This avoids the need to reload the plugin constantly
+	 * to get the newest data into the plugin.
+	 * @return {@link YamlConfiguration}
+	 */
 	public YamlConfiguration getConfig() { return YamlConfiguration.loadConfiguration(new File(plugin.getDataFolder(), "config.yml")); }
 	
+	/**
+	 * Similar to the {@link #getConfig()} method, this obtains a new instance of the file in the format
+	 * of YML. If the file is not of a YML format, then this will throw an exception of an invalid format.
+	 * @param file - YML file to load
+	 * @return {@link YamlConfiguration}
+	 */
 	public YamlConfiguration getCustomConfig(File file) { return YamlConfiguration.loadConfiguration(file); }
 	
+	/**
+	 * Completes the same action as the {@link #getCustomConfig(File)}, but allows inputting the file path
+	 * and file name of the path.
+	 * @param path - Folder to get from
+	 * @param fileName - Name of file.
+	 * @return {@link YamlConfiguration}
+	 */
 	public YamlConfiguration getCustomConfig(String path, String fileName) {
 		if(path == null || fileName == null) return null;
 		
@@ -131,6 +164,12 @@ public abstract class PluginManager<J extends JavaPlugin> {
 		}
 	}
 	
+	/**
+	 * Completes the same action as {@link #getCustomConfig(String, String)}.
+	 * @param parent - Folder to get file from.
+	 * @param fileName - Name of file.
+	 * @return {@link YamlConfiguration}
+	 */
 	public YamlConfiguration getCustomConfig(File parent, String fileName) {
 		if(parent == null || fileName == null) return null;
 		
@@ -151,39 +190,109 @@ public abstract class PluginManager<J extends JavaPlugin> {
 		}
 	}
 	
-	public void createFile(InputStream fis, File folder, String fileName) {
-		createFile(fis, new File(folder, fileName));
-	}
-	
+	/**
+	 * Registers a class instance into the ServiceManager. This registers the instance under the
+	 * plugin that is stored in this class, not the Core Plugin.<br>
+	 * Ex. registerClass(myClass.class, myClass, ServicePriority.normal);
+	 * @param clazz - {@link Class}, can be an Interface of class if classes implements an interface.
+	 * @param instance - object of clazz, instance of class.
+	 * @param priority - Sets the priority of the registered class.
+	 */
 	public <T> void registerClass(Class<T> clazz, T instance, ServicePriority priority) {
 		getPlugin().getServer().getServicesManager().register(clazz, instance, plugin, priority);
 	}
 	
+	/**
+	 * Obtains the RSP of the given class. Note that if the class is not registered, then
+	 * this will return null.
+	 * @param clazz - RSP of class to obtain.
+	 * @return {@link RegisteredServiceProvider}, otherwise null
+	 */
 	public <C> RegisteredServiceProvider<C> getServiceProvider(Class<C> clazz) {
 		return plugin.getServer().getServicesManager().getRegistration(clazz);
 	}
 	
+	/**
+	 * Gets the Registered class stored witin the RSP.
+	 * @param clazz - Class instance to obtain stored within the RSP.
+	 * @return Object of class.
+	 */
 	public <C> C getRegisteredClass(Class<C> clazz) {
 		return getServiceProvider(clazz).getProvider();
 	}
 	
+	/**
+	 * Gets the RSP of the given class. Note that if the class is not registered, then this
+	 * will return a null value.
+	 * @param clazz - Gets the RSP of the class given.
+	 * @return RSP of the Class.
+	 */
 	public static <C> RegisteredServiceProvider<C> getService(Class<C> clazz) {
 		return Bukkit.getServer().getServicesManager().isProvidedFor(clazz) ?
 				Bukkit.getServer().getServicesManager().getRegistration(clazz) : null;
 	}
 	
+	/**
+	 * Gets the object of the class stored within the RSP.
+	 * @param clazz - Class to object object of.
+	 * @return Object of class.
+	 */
 	public static <C> C getProvider(Class<C> clazz) {
 		return getService(clazz).getProvider();
 	}
 	
+	/**
+	 * Gets the Message that can be used to display when the plugin is being enabled / disabled.
+	 * @return String array
+	 */
 	public String[] getHeaderMessage() { return headerMessage; }
 	
+	/**
+	 * Gets the Message that can be used to display on the bottom of the plugin when
+	 * being enabled / disabled.
+	 * @return String array
+	 */
 	public String[] getFooterMessage() { return footerMessage; }
 	
+	/**
+	 * Sets the Header message to the given String Array
+	 * @param message - String array to set.
+	 */
 	public void setHeaderMessage(String[] message) { headerMessage = message; }
 	
+	/**
+	 * Sets the Footer message to the given String Array.
+	 * @param message - String array to set.
+	 */
 	public void setFooterMessage(String[] message) { footerMessage = message; }
 	
+	/**
+	 * Creates a new file by the inputed stream file information. This
+	 * can be used to make a file in a folder with all the information
+	 * and comments still intact. Note that if the file already exists,
+	 * it will be overridden with the new file from this function. If
+	 * the file is <tt>Null</tt>, then nothing will be created. This
+	 * allows creating files based on the provided file within the plugin.
+	 * ie, you can create a file without losing comments.
+	 * @param fis - an input stream of a file to copy data from.
+	 * @param folder - File to use as the path to File.
+	 * @param fileName - Name of File
+	 */
+	public void createFile(InputStream fis, File folder, String fileName) {
+		createFile(fis, new File(folder, fileName));
+	}
+	
+	/**
+	 * Creates a new file by the inputed stream file information. This
+	 * can be used to make a file in a folder with all the information
+	 * and comments still intact. Note that if the file already exists,
+	 * it will be overridden with the new file from this function. If
+	 * The file is <tt>Null</tt>, then nothing will be created. This
+	 * allows creating files based on the provided file within the plugin.
+	 * ie, you can create a file without losing comments.
+	 * @param fis - an input stream of a file to copy data from.
+	 * @param toLoad - file to have data in.
+	 */
 	public void createFile(InputStream fis, File toLoad) {
 		if(fis == null || toLoad == null) {
 			logMessage(Level.WARNING, "ERROR: Could not create file (" + toLoad.getName() + ") as either the"
@@ -213,12 +322,26 @@ public abstract class PluginManager<J extends JavaPlugin> {
 		}
 	}
 	
+	/**
+	 * Logs the Exception to the Console via the PrintStackTrace from the Thowable class.
+	 * A 'header' and 'footer' are also logged to help organize all the text within.
+	 * @param e - Exception to log.
+	 */
 	public void logException(Exception e) {
 		logMessage(Level.WARNING, "======== Stacktrace ========");
 		e.printStackTrace();
 		logMessage(Level.WARNING, "============================");
 	}
 	
+	/**
+	 * Logs the exception to a text file. The file is stored in the <b>Logs</b> folder
+	 * within the Plugin Folder. A new Folder, if not already created will be created
+	 * called 'Logs'. The name of the file will be the <b>date and time</b> the exception
+	 * occurred. The format of the name is: <i>YYYY-MM-DD-HMS</i>. This is good if the Server
+	 * is having issuses with the plugin and can be given to the Developer(s) to find and
+	 * fix the issue.
+	 * @param e - Exception to log into file.
+	 */
 	public void logExceptionToFile(Exception e) {
 		File logFolder = FileManager.getFolder(plugin.getDataFolder() + "/Logs");
 		File toLog = new File(logFolder, getDate() + ".txt");
@@ -231,12 +354,18 @@ public abstract class PluginManager<J extends JavaPlugin> {
 			fw.close();
 		} catch (IOException e1) {
 			logException(e1);
-			getConsole().sendMessage("&cError, could not write error data to file, another error occurred.");
+			getConsole().sendMessage("§cError, could not write error data to file, another error occurred.");
 		}
 	}
 	
+	/**
+	 * Obtains the Console in the form of a Mineraft Sender which allows the
+	 * Minecraft Color Codes to be used.
+	 * @return {@link ConsoleCommandSender}
+	 */
 	public ConsoleCommandSender getConsole() { return plugin.getServer().getConsoleSender(); }
 	
+	// Used for formating the date used in the LogExceptionToFile method.
 	private String getDate() {
 		String toReturn = "";
 		toReturn += Calendar.YEAR + "-" + Calendar.MONTH + "-" + Calendar.DAY_OF_MONTH + "-" + Calendar.HOUR_OF_DAY + Calendar.MINUTE + Calendar.SECOND;
