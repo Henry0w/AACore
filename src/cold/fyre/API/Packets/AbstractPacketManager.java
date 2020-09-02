de2b78b9eee9318dfdcdc5a7c3f63f38c6800d41
user <user@Sommod>
2020-09-01 23:16:21 -0400

    Contains the PacketManager methods

---
+++ src/cold/fyre/API/Packets/AbstractPacketManager.java
@@ -0,0 +1,71 @@
+package cold.fyre.API.Packets;
+
+import org.bukkit.Server;
+
+/**
+ * Contains a list of different methods that use packets to which are sent to either
+ * the server of player. The methods found here are SOME of the commonly used actions
+ * from the NMS classes, but NOT all. Each method will have details as to what version
+ * of Minecraft the method can be used with. Some methods may not be eligible on the
+ * current version of Minecraft the server may run. Most of these methods do not allow you to
+ * directly interact with the packets, but input data and have the code handle the rest.
+ * Some methods do return manipulatable objects.
+ * 
+ * @author Armeriness
+ * @author Sommod
+ * @since 2.0
+ *
+ */
+public abstract class AbstractPacketManager {
+	
+	private Server server;
+	private ServerVersion serverVersion;
+	//private IcyHot manager;
+	
+	/**
+	 * Contains the Server version of the server. Note that some of the
+	 * versions may not be present, this is because many of the versions
+	 * share the same class paths, i.e. the same NMS classes.
+	 * 
+	 * @author Armeriness
+	 * @author Sommod
+	 * @since 2.0
+	 *
+	 */
+	public enum ServerVersion {
+		V1_8("1.8", 1.8), V1_8_8("1.8.8", 1.88), V1_9("1.9", 1.9), V1_9_4("1.9.4", 1.94), V1_10_2("1.10.2", 1.102),
+		V1_11_2("1.11.2", 1.112), V1_12_2("1.12.2", 1.122), V1_13("1.13", 1.13), V1_13_2("1.13.2", 1.132),
+		V1_14_4("1.14.4", 1.144), V1_15_1("1.15.1", 1.151), VERROR("Error", 0.0);
+		private String name;
+		private double num;
+		
+		private ServerVersion(String name, double num) {
+			this.name = name;
+			this.num = num;
+		}
+		
+		/**
+		 * Returns the String representation of the server version. Note
+		 * that the String version will have the decimal points, i.e.
+		 * V1_15_1 will return "1.15.1".
+		 */
+		@Override
+		public String toString() { return name; }
+		
+		/**
+		 * The number will return a double of the version, i.e. V1_15_1
+		 * will return "1.151".
+		 * @return
+		 */
+		public double toNumber() { return num; }
+	}
+	
+	public AbstractPacketManager(Server server, ServerVersion version/**, IcyHot coldfyre*/) {
+		this.server = server;
+		serverVersion = version;
+		//manager = coldfyre;
+	}
+	
+	public Server getServer() { return server; }
+
+}
