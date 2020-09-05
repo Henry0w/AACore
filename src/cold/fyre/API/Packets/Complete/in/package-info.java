c48d38608e7353d1e6e4292c481db5272f7e6db7
user <user@Sommod>
2020-09-04 23:46:38 -0400

    Contains information about classes within this package

---
+++ src/cold/fyre/API/Packets/Complete/in/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Contains the packets of playIn
+ */
+/**
+ * Contains the classes of all PacketPlayIn's. 
+ * 
+ * @author Armeriness
+ * @author Sommod
+ * @since 2.0
+ *
+ */
+package cold.fyre.API.Packets.Complete.in;
---
+++ src/cold/fyre/API/Packets/Complete/out/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Contains the Classes of packets for PacketPlayOut
+ */
+/**
+ * Contains the files that are apart of the PacketPlayOut Collection.
+ * 
+ * @author Armeriness
+ * @author Sommod
+ * @since 2.0
+ *
+ */
+package cold.fyre.API.Packets.Complete.out;
