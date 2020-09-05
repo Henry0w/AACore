f0c3ffc7ebc0e0176963b9c7dadbd885c11a39bf
user <user@Sommod>
2020-09-05 01:31:36 -0400

    Used for sending basic player abilities to the player

--- src/cold/fyre/API/Packets/Complete/out/PacketPlayOutAbilities.java
+++ src/cold/fyre/API/Packets/Complete/out/PacketPlayOutAbilities.java
@@ -1,16 +1,36 @@
 package cold.fyre.API.Packets.Complete.out;
 
 import cold.fyre.API.PluginManager;
-import cold.fyre.API.Packets.Complete.AbstractPacket;
+import cold.fyre.API.Packets.Complete.PacketPlayOut;
+import cold.fyre.API.Packets.Complete.PlayerAbilities;
 
-public class PacketPlayOutAbilities<P extends PluginManager<?>> extends AbstractPacket<P> {
-
-	public PacketPlayOutAbilities(final P pluginManager) {
-		String temp = "";
+public class PacketPlayOutAbilities<P extends PluginManager<?>> extends PacketPlayOut<P> {
+	
+	private PlayerAbilities abilities;
+	
+	public PacketPlayOutAbilities(PlayerAbilities abilities) {
+		this(null, abilities);
 	}
 	
-	private PacketPlayOutAbilities(String packetName, Object packet, P pluginManager) {
-		super(packetName, packet, pluginManager);
+	public PacketPlayOutAbilities(P pluginManager, PlayerAbilities abilities) {
+		super("PacketPlayOutAbilities", pluginManager);
+		this.abilities = abilities;
+		createPacket();
+	}
+
+	@Override
+	protected void createPacket() {
+		// Stops the rest of the code if the object is null. This will
+		// Still throw and error if the sendPacket is invoked, but
+		// This stops unnecessary code from being ran.
+		if(abilities == null) { 
+			loadPacket(null);
+			return;
+		}
+		
+		// TODO: Create an NBTTagCompound object
+		// TODO: Create a PacketPlayerOutAbilities packet
+		// TODO: Store packet in super class,
 	}
 
 }
