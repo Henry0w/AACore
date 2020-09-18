8bb88702ab2d30c95c9a744457a40dd55353c519
user <user@192.168.0.105>
2020-09-18 17:41:11 -0400

    Added comments

--- src/cold/fyre/API/Utilities.java
+++ src/cold/fyre/API/Utilities.java
@@ -2,8 +2,12 @@ package cold.fyre.API;
 
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
+import java.util.Set;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 
 /**
@@ -36,6 +40,12 @@ public class Utilities {
 		/** Format: $1234.56 */ CURRENCY, /** Format: $1,234.56 */ STANDARD;
 	}
 	
+	/**
+	 * Formats the number given into two standards methods of number format.
+	 * @param number - Number to format
+	 * @param format - Format type
+	 * @return String of number
+	 */
 	public static String formatNumber(double number, Currency format) {
 		DecimalFormat stand = new DecimalFormat("#,###.##");
 		DecimalFormat money = new DecimalFormat("#0.00");
@@ -56,6 +66,12 @@ public class Utilities {
 		return toReturn;
 	}
 	
+	/**
+	 * Wraps the string into an array based on the amount of words per line.
+	 * @param toWarp - String to word-wrap
+	 * @param words - amount of words per line
+	 * @return Array of string
+	 */
 	public static String[] wordWrapper(String toWarp, int words) {
 		String[] toReturn = new String[(int) Math.ceil(toWarp.split(" ").length / (double) words)];
 		String[] indiv = toWarp.split(" ");
@@ -73,6 +89,13 @@ public class Utilities {
 		return toReturn;
 	}
 	
+	/**
+	 * Replaces the string color code symbol with the correct code depending on the 
+	 * use of the text.
+	 * @param format - Format type of string
+	 * @param toReturn - String to format.
+	 * @return Formated string
+	 */
 	public static String replace(Chat format, String toReturn) {
 		if(format == Chat.CHAT)
 			return toReturn.replace('&', '§');
@@ -80,6 +103,13 @@ public class Utilities {
 			return toReturn.replaceAll("&", format.toString());
 	}
 	
+	/**
+	 * Replaces the string color code symbol with the correct code depending on the use of the text.
+	 * Each line is checked and replaced.
+	 * @param format - Format type
+	 * @param toReturn - Array to format
+	 * @return Formated string array
+	 */
 	public static String[] replace(Chat format, String[] toReturn) {
 		for(int line = 0; line < toReturn.length; line++)
 			toReturn[line] = (format == Chat.CHAT) ? toReturn[line].replace('&', '§') : toReturn[line].replaceAll("&", format.toString());
@@ -87,6 +117,14 @@ public class Utilities {
 		return toReturn;
 	}
 	
+	/**
+	 * Gets the location of the direction from the starting location. The direction is based on the entity
+	 * or direction the location given. The offset gets the amount of blocks in the direction.
+	 * @param start - Location to start from
+	 * @param offset - Amount of blocks into the direction
+	 * @param direction - Where to get the new location of which way
+	 * @return Location of in the new direction
+	 */
 	public static Location getOffSetLocation(Location start, double offset, Direction direction) {
 		if(offset < 0.01D || start == null)
 			return start;
@@ -104,5 +142,14 @@ public class Utilities {
 		
 		return start.add(dir.multiply(offset));
 	}
+	
+	/**
+	 * Gets the block the player is looking at or within the distance given. If the player
+	 * is not looking at a block within the distance, then this will return null.
+	 * @param player - Player to get block of.
+	 * @param distance - Number of blocks to check
+	 * @return Block of player looking at.
+	 */
+	public Block getTargetBlock(Player player, int distance) { return player.getTargetBlock((Set<Material>) null, distance); }
 
 }
