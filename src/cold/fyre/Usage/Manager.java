4c1817bb336f01b53fb5c79d03c54ad4b1c46791
Armeriness <jm0350@yahoo.com>
2020-09-28 13:52:01 -0400

    Added override methods

--- src/cold/fyre/Usage/Manager.java
+++ src/cold/fyre/Usage/Manager.java
@@ -4,9 +4,26 @@ import cold.fyre.IcyHot;
 import cold.fyre.API.PluginManager;
 
 public class Manager extends PluginManager<IcyHot> {
+	
+	//PacketManager packetManager;
 
 	public Manager(IcyHot plugin) {
 		super(plugin);
+	}
+	
+	@Override
+	public void onStartup() {
+		super.onStartup();
+	}
+	
+	@Override
+	public void onShutdown() {
+		super.onShutdown();
+	}
+	
+	@Override
+	public boolean onReload() {
+		return super.onReload();
 	}
 
 }
