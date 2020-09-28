66d7982515b5699f6dd6c22ee10f5cef37d52cab
Sommod <user@192.168.0.105>
2020-09-28 19:48:56 -0400

    Updated Package information

--- src/cold/fyre/API/Packets/package-info.java
+++ src/cold/fyre/API/Packets/package-info.java
@@ -2,8 +2,9 @@
  * Contains all data via packet handling for NMS classes
  */
 /**
- * Contains all the classes that are used for NMS classes, excluding the {@link PacketManager} class which
- * is found in the parent package of this.
+ * Contains abstract classes that allow using NMS classes and objects. The classes are all abstract to force
+ * a sub-class. This is to remove the need to constantly have to put type parameters for every packet that is
+ * being created.
  * 
  * @author Armeriness
  * @author Sommod
