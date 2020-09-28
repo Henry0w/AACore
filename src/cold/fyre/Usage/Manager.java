66e5591e3cb63ca06d4d5a31274b5835f53bc6c4
user <user@192.168.0.105>
2020-09-28 05:07:04 -0400

    Added PluginManager class for AACore

---
+++ src/cold/fyre/Usage/Manager.java
@@ -0,0 +1,12 @@
+package cold.fyre.Usage;
+
+import cold.fyre.IcyHot;
+import cold.fyre.API.PluginManager;
+
+public class Manager extends PluginManager<IcyHot> {
+
+	public Manager(IcyHot plugin) {
+		super(plugin);
+	}
+
+}
