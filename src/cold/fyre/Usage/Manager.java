4b474fcabbbeba96e77d2309141b23d81eed9594
user <user@192.168.0.105>
2020-09-30 03:23:12 -0400

    Added PacketManager initialization

--- src/cold/fyre/Usage/Manager.java
+++ src/cold/fyre/Usage/Manager.java
@@ -1,11 +1,15 @@
 package cold.fyre.Usage;
 
+import org.bukkit.plugin.ServicePriority;
+
 import cold.fyre.IcyHot;
+import cold.fyre.API.PacketManager;
 import cold.fyre.API.PluginManager;
+import cold.fyre.API.Packets.AbstractPacketManager.ServerVersion;
 
 public class Manager extends PluginManager<IcyHot> {
 	
-	//PacketManager packetManager;
+	private PacketManager packetManager;
 
 	public Manager(IcyHot plugin) {
 		super(plugin);
@@ -13,7 +17,7 @@ public class Manager extends PluginManager<IcyHot> {
 	
 	@Override
 	public void onStartup() {
-		super.onStartup();
+		initPacketManager();
 	}
 	
 	@Override
@@ -24,6 +28,24 @@ public class Manager extends PluginManager<IcyHot> {
 	@Override
 	public boolean onReload() {
 		return super.onReload();
+	}
+	
+	@Override
+	public PacketManager getPacketManager() {
+		return packetManager;
+	}
+	
+	// Initializes the PacketManager class and registers it
+	// within the RegisteredServiceProvider
+	private void initPacketManager() {
+		String version = getPlugin().getServer().getClass().getPackage().toString().split("\\.")[3];
+		
+		for(ServerVersion sv : ServerVersion.values()) {
+			if(sv.toPackageString().equalsIgnoreCase(version)) {
+				packetManager = new PacketManager(getPlugin().getServer(), sv, this);
+				registerClass(PacketManager.class, packetManager, ServicePriority.Normal);
+			}
+		}
 	}
 
 }
