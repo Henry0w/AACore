96c6192c1e855f0603031a7e00cc9bb3ae2ecd19
Armeriness <jm0350@yahoo.com>
2020-10-14 10:23:46 -0400

    Added handler for AACore commands

---
+++ src/cold/fyre/CMD/Commands/CommandHandler.java
@@ -0,0 +1,14 @@
+package cold.fyre.CMD.Commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class CommandHandler implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		return false;
+	}
+
+}
