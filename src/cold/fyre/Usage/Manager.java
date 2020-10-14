2badb069ffc04e1428feb93d914a90dd321a92b0
Armeriness <jm0350@yahoo.com>
2020-10-14 15:17:09 -0400

    Initialized all attributes of plugin

--- src/cold/fyre/Usage/Manager.java
+++ src/cold/fyre/Usage/Manager.java
@@ -1,7 +1,13 @@
 package cold.fyre.Usage;
 
+import java.io.File;
+
+import org.bukkit.event.HandlerList;
+
 import cold.fyre.IcyHot;
 import cold.fyre.API.PluginManager;
+import cold.fyre.CMD.CommandHandler;
+import cold.fyre.Events.DeveloperMessage;
 
 public class Manager extends PluginManager<IcyHot> {
 
@@ -14,21 +20,42 @@ public class Manager extends PluginManager<IcyHot> {
 	@Override
 	public void onStartup() {
 		logMessage(getHeaderMessage());
-		logMessage("Running pre-check...");
+		//logMessage("Running pre-check...");
 		checkPlugins();
 		
+		//logMessage("Pre-check complete");
+		logMessage("Registering Config files...");
+		registerConfigs();
+		
+		logMessage("Config files loaded");
+		logMessage("Registering Commands...");
+		new CommandHandler(this);
+		
+		logMessage("Commands Registered");
+		logMessage("Registering Events...");
+		new DeveloperMessage(this);
+		
+		logMessage("Events registered");
 		logMessage(getFooterMessage());
 	}
 	
 	@Override
 	public void onShutdown() {
 		logMessage(getHeaderMessage());
+		logMessage("Shutting down...");
+		HandlerList.unregisterAll(getPlugin());
+		logMessage("Shutdown complete");
 		logMessage(getFooterMessage());
 	}
 	
-	@Override
-	public boolean onReload() {
-		return super.onReload();
+	private void registerConfigs() {
+		if(!getPlugin().getDataFolder().exists())
+			getPlugin().getDataFolder().mkdir();
+		
+		File config = new File(getPlugin().getDataFolder(), "config.yml");
+		
+		if(!config.exists())
+			createFile(getPlugin().getClass().getResourceAsStream("/Resources/config.yml"), config);
 	}
 
 }
