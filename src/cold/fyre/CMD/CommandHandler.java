59e40bbd7570259881d895e18376fab04c8b60ec
Armeriness <jm0350@yahoo.com>
2020-10-14 14:54:52 -0400

    Added command handler

--- src/cold/fyre/CMD/CommandHandler.java
+++ src/cold/fyre/CMD/CommandHandler.java
@@ -4,6 +4,10 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
+import cold.fyre.API.Utilities;
+import cold.fyre.CMD.Commands.CommandDisable;
+import cold.fyre.CMD.Commands.CommandEnable;
+import cold.fyre.CMD.Commands.CommandHelp;
 import cold.fyre.CMD.Commands.CommandVersion;
 import cold.fyre.Usage.Manager;
 
@@ -24,9 +28,30 @@ public class CommandHandler implements CommandExecutor {
 			case 0:
 				new CommandVersion(sender, command, args, manager);
 				return true;
+			
+			case 1:
+				if(Utilities.isAny(args[0], "help", "h"))
+					new CommandHelp(sender, command, args, manager);
+				else if(Utilities.isAny(args[0], "list", "l"))
+					new CommandEnable(sender, command, args, manager);
+				else
+					sender.sendMessage("§c§lAACore §8§l>> §7Error, invalid arguments given.");
+				return true;
+			
+			case 2:
+				if(Utilities.isAny(args[0], "help", "h"))
+					new CommandHelp(sender, command, args, manager);
+				else if(Utilities.isAny(args[0], "enable", "e"))
+					new CommandEnable(sender, command, args, manager);
+				else if(Utilities.isAny(args[0], "disable", "d"))
+					new CommandDisable(sender, command, args, manager);
+				else
+					sender.sendMessage("§c§lAACore §8§l>> §7Error, invalid arguments given.");
+				return true;
 
 			default:
-				break;
+				sender.sendMessage("§c§lAACore §8§l>> §7Error, unknown command.");
+				return false;
 			}
 		}
 		
