e08eef48a8d7587437c1735a588eaa64abb2cad5
Sommod <Sommod@192.168.0.105>
2020-12-10 13:10:12 -0500

    Added Interface for packets

--- src/cold/fyre/API/Packets/AAPacket.java
+++ src/cold/fyre/API/Packets/AAPacket.java
@@ -1,161 +1,46 @@
 package cold.fyre.API.Packets;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.util.Arrays;
-import java.util.List;
-
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
-
-import cold.fyre.API.FileManager;
-import cold.fyre.API.PluginManager;
+import cold.fyre.API.Managers.FileManager;
 
 /**
- * Class that holds and handles NMS classes and objects. This class
- * is forced to be extended, otherwise the class would contain several
- * layers of Type parameters. The extended class does not need to possess
- * or override any methods, just forced to remove the constant need for
- * using Type parameters every time a packet is being created.
+ * Class that is used to handle classes of NMS. This uses reflection
+ * to access and use the packets. As such, the normal methods will not
+ * appear. To use any of the methods found within the class, you need
+ * to know what the method name is, including any capitalization, what
+ * objects the method takes, and what type of objects the methods take.
+ * Once all the information is given, you can grab the object from the
+ * return method.
  * 
  * @author Armeriness
  * @author Sommod
  * @since 2.0
  *
- * @param <P>
  */
-public abstract class AAPacket<P extends PluginManager<?>> extends AbstractPacket<P> {
-
-	/**
-	 * Creates an object that holds the Class name of the NMS class. This can be used to create
-	 * the NMS class of the specified version of the server via reflection. While using this is
-	 * not thread-safe, it allows the removal of handling version-specific actions. The packet
-	 * in which this object will hold and handle is given by the <b>packetName</b>. Please note
-	 * that the string object is case-sensitive.
-	 * @param packetName - NMS class name
-	 * @param pluginManager - Your PluginManager
-	 */
-	public AAPacket(String packetName, P pluginManager) { super(packetName, pluginManager); }
+public interface AAPacket {
 	
-	/**
-	 * Creates an object that holds the Class name of the NMS class. This can be used to create
-	 * the NMS class of the specified version of the server via reflection. While using this is
-	 * not thread-safe, it allows the removal of handling version-specific actions. The packet
-	 * in which this object will hold and handle is given by the <b>packetName</b>. Please note
-	 * that the string object is case-sensitive.
-	 * @param packetName - NMS class name
-	 * @param plugin - Your Plugin or sub-class of JavaPlugin
-	 */
-	public AAPacket(String packetName, JavaPlugin plugin) { super(packetName, plugin); }
-	
-	/**
-	 * Activates a method within the packet with the given parameters. The method is also case-sensitive.
-	 * @param method - Method to run
-	 * @param types - Parameter class types
-	 * @param parameters - Object(s) to pass
-	 */
-	public void runFunction(String method, Class<?>[] types, Object[] parameters) {
-		try {
-			getPacket().getClass().getMethod(method, types).invoke(getPacket(), parameters);
-		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException
-				| SecurityException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	/**
-	 * Sets the given field to the object given. Note that if you try to set the field
-	 * to an incompatible object, (ie. setting a boolean to a numerical value), then this
-	 * will cause an error. The field is case-sensitive.
-	 * @param fieldName- Case-sensitive name of variable
-	 * @param toSet - Object to set field to
-	 */
-	public void setField(String fieldName, Object toSet) {
-		Field field;
+	String getName();
+	Object getPacket();
+	Object runMethod(String methodName, Object... parameters);
+	Object getField(String fieldName);
+	void setField(String fieldName, Object value);
+	static Enum<?> getEnum(String packetClass) {
+		Enum<?> toReturn = null;
 		
 		try {
-			field = getPacket().getClass().getField(fieldName);
-			field.setAccessible(true);
-			field.set(getPacket(), toSet);
-		} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	/**
-	 * Obtain the object value of the field of the Object class given.
-	 * @param field - Field of Object class
-	 * @param toGet - Object to get another Object from
-	 * @return Value of Field
-	 */
-	public Object getField(String field, Object toGet) {
-		Field toReturn;
-		
-		try {
-			toReturn = getPacket().getClass().getDeclaredField(field);
-			return toReturn.get(toGet);
-		} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
-			FileManager.logExceptionToFile(getBasePlugin().getName(), e);
-			return null;
-		}
-	}
-	
-	/**
-	 * Attempts to get the Enum Values of the Given class. Note that this will not allow you to obtain
-	 * any field members found within the class or obtain any data attached to the Enum Value. This
-	 * only gets the Enum itself. If the Enum value is not found, or an error occurs, then this will
-	 * return null.
-	 * @param enumClass - Enum class.
-	 * @param enumString - String representation of Enum value.
-	 * @return Enum as an Object.
-	 */
-	public Object getEnumValues(Class<?> enumClass, String enumString) {	
-		try {
-			List<?> listOfNames = Arrays.asList(enumClass.getEnumConstants());
+			Class<?> packetClazz = Class.forName(packetClass);
 			
-			for(Object obj :  listOfNames) {
-				if(obj.toString().equalsIgnoreCase(enumString))
-					return obj;
+			if(packetClazz.isEnum()) {
+				toReturn = (Enum<?>) packetClazz.cast(Enum.class);
 			}
-		} catch (SecurityException | IllegalArgumentException | ClassCastException e) {
-			FileManager.logExceptionToFile(getBasePlugin().getName(), e);
-			return null;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			FileManager.logExceptionToFile(null, e);
 		}
 		
-		return null;
-		
+		return toReturn;
 	}
 	
-	/**
-	 * Sends this packet to the player. Note that the packet, if not done correctly or given null values
-	 * in incorrect places will cause this to throw an exception error and log the exception either in
-	 * the plugin folder of the plugin that tried to run this file, or in the generic folder found
-	 * within the main Plugins folder called <b>Error Logs</b>.
-	 * @param toSend - Player to send packet to.
-	 */
-	public void sendPacket(Player toSend) {
-		try {
-			Object handle = toSend.getClass().getMethod("getHandle").invoke(toSend);
-			Object playerConnection = handle.getClass().getField("playerConnection").get(handle);
-			
-			playerConnection.getClass().getMethod("sendPacket", getPacketClass("Packet")).invoke(playerConnection, getPacket());
-		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException | NoSuchFieldException | NullPointerException e) {
-			FileManager.logExceptionToFile(getBasePlugin().getName(), e);
-			return;
-		}
-	}
-	
-	/**
-	 * Checks if a packet for this object has been created yet or not.
-	 * @return True - if packet has been created
-	 */
-	public boolean isCreated() { return getPacket() != null; }
-	
-	/**
-	 * Returns the Name of this packet. This is the NMS class that
-	 * is stored within this object. Note that the packet may not
-	 * be initialized, but the name is still stored.
-	 */
-	public String toString() { return getPacketName(); }
+	Exception getError();
+	boolean hasError();
 
 }
