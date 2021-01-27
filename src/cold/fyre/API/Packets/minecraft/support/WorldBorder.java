2077e20d4595b598501535419aab3e3b60e272f8
Sommod <Sommod@192.168.0.105>
2021-01-27 00:13:16 -0500

    Added block position getter

--- src/cold/fyre/API/Packets/minecraft/support/WorldBorder.java
+++ src/cold/fyre/API/Packets/minecraft/support/WorldBorder.java
@@ -8,6 +8,7 @@ public class WorldBorder {
 	
 	private double centerX, centerZ, damageAmount, damageBuffer, size;
 	private int warningDistance, warningTime;
+	private BlockPosition position;
 	
 	public WorldBorder() { this(0, 0, 0.5D, 0.2D, 29999984, 4, 2); }
 	
@@ -19,6 +20,7 @@ public class WorldBorder {
 		this.damageAmount = damageAmount;
 		this.warningDistance = warningDistance;
 		this.warningTime = warningTime;
+		this.position = center;
 	}
 	
 	public WorldBorder(double centerX, double centerZ, double damageAmount, double damageBuffer, double size, int warningDistance, int warningTime) {
@@ -29,6 +31,7 @@ public class WorldBorder {
 		this.size = size;
 		this.warningDistance = warningDistance;
 		this.warningTime = warningTime;
+		position = new BlockPosition(centerX, 64, centerZ);
 	}
 	
 	public WorldBorder(WorldBorder border) {
@@ -69,6 +72,8 @@ public class WorldBorder {
 	public int getWarningDistance() { return warningDistance; }
 	
 	public int getWarningTime() { return warningTime; }
+	
+	public BlockPosition getBlockPosition() { return position; }
 	
 	public void setCenter(double x, double z) {
 		centerX = x;
