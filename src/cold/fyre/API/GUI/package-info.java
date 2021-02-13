a19f233adc8964c6b016a5003784db926036cfed
Sommod <Sommod@192.168.0.105>
2021-02-12 23:55:14 -0500

    Added Package information about GUI's

---
+++ src/cold/fyre/API/GUI/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Gui Handling
+ */
+/**
+ * Handles aspects of GUI interfaces, such as chests, anvils, etc...
+ * 
+ * @author Armeriness
+ * @author Sommod
+ * @since 2.1
+ *
+ */
+package cold.fyre.API.GUI;
