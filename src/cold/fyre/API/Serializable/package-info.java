d2675367eb0b2ff8d212b356dcf917e13ab0af7b
Sommod <Sommod@192.168.0.105>
2021-04-05 14:10:49 -0400

    Added Serialization

---
+++ src/cold/fyre/API/Serializable/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * Handles the serialization of Bukkit Objects
+ */
+/**
+ * The classes pertained within this package contain
+ * methods to make the Bukkit objects serializable and
+ * capable of being streamed into a file or have a
+ * serialized object de-serialized.
+ * 
+ * @author Armeriness
+ * @author Sommod
+ * @since 2.2
+ *
+ */
+package cold.fyre.API.Serializable;
